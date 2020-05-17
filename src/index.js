bdb91e7de64c670d24b2ad5c48add1002b401bac
Justin Cheng <ijustin17@gmail.com>
2020-05-17 18:22:53 -0500

    Added react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // Added react-router-dom just in case
+  <Router>
+    <>
+      <Switch>
+        {/*  If more routes the below segment must be last */}
+        {/* ---------------------------------------------- */}
+        <Route path="/" exact={true}>
+          {/* App.js staying for now till .jsx page is avaliable */}
+          <App />
+        </Route>
+        {/* ---------------------------------------------- */}
+      </Switch>
+    </>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
